eeaddbc55ecd7068412e91e5b5f173161169c437
raykingflamez <raykingflamez@gmail.com>
2022-06-23 00:03:13 +0100

    slight adjustment

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ let height = 6
 let country = "Nigeria"
 
 
-alert("My name is "+  Name + ", I am " + 6 + "ft and i am from " +  country)
+alert("My name is "+  Name + ".\n I'm " + 6 + "ft and ,\n I am from " +  country)
+
 
